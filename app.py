34153790d7b5cd035ea161ff1445a2c9c1fe93e8
lpollis <lpollis@phillies.com>
2025-01-26 21:04:37 -0500

    Update app.py
    Adding basic functionality for statistical inputs

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
-from dash import Dash, html
+from dash import Dash, dcc, html
 
 app = Dash()
 
 app.layout = [
-    html.Div(children='Simple WAR Calculator')
+  html.H1('Simple WAR Calculator'),
+  html.Div(children=[
+    html.Label('Home Runs'),
+    html.Br(),
+    dcc.Input(value = '16', type = 'number'),
+    html.Br(),
+    html.Br(),
+    html.Label('Walks'),
+    html.Br(),
+    dcc.Input(value = '50', type = 'number'),
+    html.Br(),
+    html.Br(),
+    html.Label('Strikeouts'),
+    html.Br(),
+    dcc.Input(value = '75', type = 'number'),
+    html.Br(),
+    html.Br(),
+    html.Label('Stolen Bases'),
+    html.Br(),
+    dcc.Input(value = '8', type = 'number'),
+    html.Br(),
+    html.Br(),
+    html.Label('BABIP'),
+    html.Br(),
+    dcc.Input(value = '.300', type = 'number', step = 0.001),
+    html.Br(),
+    html.Br(),
+    html.Label('Plate Appearances'),
+    html.Br(),
+    dcc.Input(value = '600', type = 'number'),
+    html.Br(),
+    html.Br(),
+    html.Label('Games'),
+    html.Br(),
+    dcc.Input(value = '150', type = 'number'),
+    html.Br(),
+    html.Br()
+  ])
 ]
 
 if __name__ == '__main__':
